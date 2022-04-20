4ad1a3a30d371a5450b977cf7f5dd4344dd1c078
Minal06 <94176489+Minal06@users.noreply.github.com>
2022-04-20 06:51:54 +0200

    1st UI commit
    1st part of checking if names and other inputs are working correctly.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] InputField player1Name;
+    [SerializeField] InputField player2Name;
+    [SerializeField] InputField timeSet;
+
+    public void StartGame()
+    {
+        Debug.Log("Player 1 name is:" + player1Name.text);
+        Debug.Log("Player 2 name is:" + player2Name.text);
+        Debug.Log("Game will take:" + timeSet.text);
+       // GameManager.player1NameInGame = player1Name.text;
+       // GameManager.player1NameInGame = player2Name.text;
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
