03649512ee8b0ddc1c903b2bdb00acbdc6eee947
Minal06 <94176489+Minal06@users.noreply.github.com>
2022-04-20 22:18:50 +0200

    endofaday
    end of a first few hours

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Move Keys")]
+    [SerializeField] KeyCode ForwardKey;
+    [SerializeField] KeyCode BackwardKey;
+    [SerializeField] float zRange;
+    [SerializeField] float speed;
+
+    [Header("Position Reset Place")]
+    [SerializeField] float baseX;
+    [SerializeField] float baseY;
+    [SerializeField] float baseZ;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(ForwardKey))
+        {
+            MoveUp();
+        }
+        if (Input.GetKey(BackwardKey))
+        {
+            MoveDown();
+        }
+        BackLineMax();        
+    }
+     
+
+    void MoveUp()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+    void MoveDown()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+    }
+    //players cannot move below certain point
+    void BackLineMax()
+    {
+        if (transform.position.z < zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+    }
+
+    void ResetPosition()
+    {
+        transform.position = new Vector3(baseX, baseY, baseZ);
+    }
+}
